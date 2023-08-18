df42060f4091c0bcc8c1573f4ddeb72f9c38d4e9
Bianca Aparecida Alves Araújo <biancaaparecida07@gmail.com>
2023-08-18 11:08:26 -0300

    refactor: preparação de projeto

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-const express = require('express');
-const app = express();
 const porta = 3000;
+const express = require('express');
+const rotas = require('./rotas');
+const app = express();
 
+app.use(express.json()); //middleware nativo express para transação de informação no formato json
+app.use(rotas);
 app.listen(porta);
