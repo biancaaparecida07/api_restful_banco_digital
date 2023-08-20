0fb0c4a9085a3379393ee286848fd59245fb08c4
Bianca Aparecida Alves Araújo <biancaaparecida07@gmail.com>
2023-08-20 18:45:55 -0300

    feat: endpoint Criar Conta com validação completa

--- src/controllers/contas.js
+++ src/controllers/contas.js
@@ -1,4 +1,3 @@
-const bancoDeDados = require('../../database/bancodedados');
 let { contas } = require('../../database/bancodedados')
 let numConta = 1;
 
@@ -12,16 +11,29 @@ const listarContas = (req, res) => {
 
 const criarConta = (req, res) => {
     const novaConta = req.body;
+
+    //Definição de saldo inicial 0
     const saldo = 0;
 
+    //Validação - Campos obrigatórios declarados e preenchidos no body
     if (!novaConta.nome || !novaConta.cpf || !novaConta.data_nascimento || !novaConta.telefone || !novaConta.email || !novaConta.senha) {
-        return res.status(400).json({ message: 'Campo obrigatório não preenchido. A conta não foi criada!' })
+        return res.status(400).json({ message: 'Campo obrigatório não declarado ou em branco. A conta não foi criada!' })
     }
+
+    //Validação CPF e email únicos
+    for (let i = 0; i < contas.length; i++) {
+        if ((novaConta.cpf === contas[i].usuario.cpf) || (novaConta.email === contas[i].usuario.email)) {
+            return res.status(400).json({ message: 'Já existe uma conta com o cpf ou e-mail informado!' })
+        }
+    }
+
     const novaContaCadastrar = {
         numConta: numConta,
         saldo: saldo,
         usuario: novaConta
     }
+
+    //Número único.
     numConta++;
 
     contas.push(novaContaCadastrar);
