b5beef3cd719bace21acc9691b925f71c85c6635
Bianca Aparecida Alves Araújo <biancaaparecida07@gmail.com>
2023-08-20 18:15:35 -0300

    feat: endpoint Criar Conta com validação parcial

--- src/controllers/contas.js
+++ src/controllers/contas.js
@@ -1,13 +1,35 @@
 const bancoDeDados = require('../../database/bancodedados');
+let { contas } = require('../../database/bancodedados')
+let numConta = 1;
 
 const listarContas = (req, res) => {
-    if (bancoDeDados.contas.length === 0) {
+    if (contas.length === 0) {
         return res.status(204).json();
     }
 
-    return res.status(200).json(bancoDeDados.contas);
+    return res.status(200).json(contas);
+};
+
+const criarConta = (req, res) => {
+    const novaConta = req.body;
+    const saldo = 0;
+
+    if (!novaConta.nome || !novaConta.cpf || !novaConta.data_nascimento || !novaConta.telefone || !novaConta.email || !novaConta.senha) {
+        return res.status(400).json({ message: 'Campo obrigatório não preenchido. A conta não foi criada!' })
+    }
+    const novaContaCadastrar = {
+        numConta: numConta,
+        saldo: saldo,
+        usuario: novaConta
+    }
+    numConta++;
+
+    contas.push(novaContaCadastrar);
+    return res.status(200).json();
+
 };
 
 module.exports = {
-    listarContas
+    listarContas,
+    criarConta
 }
--- src/rotas.js
+++ src/rotas.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const { listarContas } = require('./controllers/contas');
+const { listarContas, criarConta } = require('./controllers/contas');
 const { validarSenha } = require('./intermediarios');
 const rotas = express();
 
-
+/**--Endpoints--**/
+//Listar Contas
 rotas.get('/contas', validarSenha, listarContas);
+
+//Criar Conta
+rotas.post('/contas', criarConta);
+
 module.exports = rotas;
