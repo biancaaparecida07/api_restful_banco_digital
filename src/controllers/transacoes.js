192a659907c5d4a774125ce1386a8e5c5ad8c740
Bianca Aparecida Alves Araújo <biancaaparecida07@gmail.com>
2023-08-22 13:51:39 -0300

    feat: endpoint Depositar

--- src/controllers/transacoes.js
+++ src/controllers/transacoes.js
@@ -1,6 +1,36 @@
 let { contas, depositos } = require('../../database/bancodedados');
 
 const depositar = (req, res) => {
+    const { numero_conta, valor } = req.body;
+
+    //Validação - Dados disponíveis no body
+    if (!numero_conta || !valor) {
+        return res.status(400).json({ message: 'Os dados de número da conta e valor do depósito são obrigatórios!' })
+    }
+
+    //Validação - Existência da conta
+    const contaDepositar = contas.find((conta) => {
+        return (conta.numero === Number(numero_conta));
+    })
+    if (!contaDepositar) {
+        return res.status(404).json({ message: 'O número da conta informado não existe.' })
+    }
+
+    //Validação - Valor depositado
+    if (valor <= 0) {
+        return res.status(403).json({ message: 'Não é permitido depósito de valor igual ou menor que zero.' })
+    }
+    const dataDeposito = new Date();
+    const registro = {
+        data: dataDeposito,
+        numero_conta: numero_conta,
+        valor: valor
+    }
+
+    contaDepositar.saldo += valor;
+    depositos.push(registro);
+    return res.status(204).json();
+
 }
 
 const sacar = (req, res) => {
